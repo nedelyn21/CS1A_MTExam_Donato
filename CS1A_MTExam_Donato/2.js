2229cf2c1942d003b7f63045dcfbd9b7fabc08d8
nedelyn21 <nedelyndonato@gmail.com>
2025-03-21 21:11:38 +0800

    Add files via upload

---
+++ CS1A_MTExam_Donato/2.js
@@ -0,0 +1,8 @@
+// User Information Collection (Class Subjects)
+let subjectTitle = prompt("Enter subject title:");
+let classSchedule = prompt("Enter class schedule (Time, Days):");
+let classroom = prompt("Enter classroom:");
+let classInstructor = prompt("Enter class instructor:");
+let studentName = prompt("Enter your name:");
+
+console.log("${Nedelyn Donato} is currently enrolled in ${DSA} with a class schedule of ${3-4 Pm, MTTHF} at room ${CLB}. The instructor for the subject is $ {O. Pascua}.");
