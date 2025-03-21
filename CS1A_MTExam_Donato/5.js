6ca2b917734d01095abacd87546b26401f570184
nedelyn21 <nedelyndonato@gmail.com>
2025-03-21 21:13:10 +0800

    Add files via upload

---
+++ CS1A_MTExam_Donato/5.js
@@ -0,0 +1,8 @@
+// Color Preferences
+let favColors = [];
+
+for (let i = 0; i < 3; i++){
+    let color = prompt("Enter color ${i + 1}:");
+    favColors.push(color);
+    console.log("Updated colors:", favColors);
+}
